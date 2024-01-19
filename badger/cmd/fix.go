ac73d184c3ff0813b49d15514cbbbc00628a49dd
Roman Khafizianov <requilence@gmail.com>
2024-01-19 21:33:58 +0100

    fix dir copy impl

--- badger/cmd/fix.go
+++ badger/cmd/fix.go
@@ -18,17 +18,14 @@ package cmd
 
 import (
 	"fmt"
-	"io"
 	"math"
 	"os"
-	"path/filepath"
 	"strings"
-	"syscall"
 	"time"
 
-	"github.com/spf13/cobra"
-
 	"github.com/dgraph-io/badger/v4"
+	cp "github.com/otiai10/copy"
+	"github.com/spf13/cobra"
 )
 
 var fof = struct {
@@ -111,11 +108,8 @@ func removeEmptyTables(cmd *cobra.Command, args []string) error {
 	}
 	// copy dir recursively
 	fmt.Printf("Creating backup from %s to %s\n", opt.Dir, fof.backupDir)
-	err = CreateIfNotExists(fof.backupDir, 0755)
-	if err != nil {
-		return fmt.Errorf("unable to create backup dir %s: %v", fof.backupDir, err)
-	}
-	err = CopyDirectory(opt.Dir, fof.backupDir)
+
+	err = cp.Copy(opt.Dir, fof.backupDir)
 	if err != nil {
 		return fmt.Errorf("unable to backup database: %v", err)
 	}
@@ -134,111 +128,4 @@ func removeEmptyTables(cmd *cobra.Command, args []string) error {
 	fmt.Println("Database is fixed")
 
 	return nil
-}
-
-func CopyDirectory(scrDir, dest string) error {
-	entries, err := os.ReadDir(scrDir)
-	if err != nil {
-		return err
-	}
-	for _, entry := range entries {
-		sourcePath := filepath.Join(scrDir, entry.Name())
-		destPath := filepath.Join(dest, entry.Name())
-
-		fileInfo, err := os.Stat(sourcePath)
-		if err != nil {
-			return err
-		}
-
-		stat, ok := fileInfo.Sys().(*syscall.Stat_t)
-		if !ok {
-			return fmt.Errorf("failed to get raw syscall.Stat_t data for '%s'", sourcePath)
-		}
-
-		switch fileInfo.Mode() & os.ModeType {
-		case os.ModeDir:
-			if err := CreateIfNotExists(destPath, 0755); err != nil {
-				return err
-			}
-			if err := CopyDirectory(sourcePath, destPath); err != nil {
-				return err
-			}
-		case os.ModeSymlink:
-			if err := CopySymLink(sourcePath, destPath); err != nil {
-				return err
-			}
-		default:
-			if err := Copy(sourcePath, destPath); err != nil {
-				return err
-			}
-		}
-
-		if err := os.Lchown(destPath, int(stat.Uid), int(stat.Gid)); err != nil {
-			return err
-		}
-
-		fInfo, err := entry.Info()
-		if err != nil {
-			return err
-		}
-
-		isSymlink := fInfo.Mode()&os.ModeSymlink != 0
-		if !isSymlink {
-			if err := os.Chmod(destPath, fInfo.Mode()); err != nil {
-				return err
-			}
-		}
-	}
-	return nil
-}
-
-func Copy(srcFile, dstFile string) error {
-	out, err := os.Create(dstFile)
-	if err != nil {
-		return err
-	}
-
-	defer out.Close()
-
-	in, err := os.Open(srcFile)
-	if err != nil {
-		return err
-	}
-
-	defer in.Close()
-
-	_, err = io.Copy(out, in)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func Exists(filePath string) bool {
-	if _, err := os.Stat(filePath); os.IsNotExist(err) {
-		return false
-	}
-
-	return true
-}
-
-func CreateIfNotExists(dir string, perm os.FileMode) error {
-	if Exists(dir) {
-		return nil
-	}
-
-	if err := os.MkdirAll(dir, perm); err != nil {
-		return fmt.Errorf("failed to create directory: '%s', error: '%s'", dir, err.Error())
-	}
-
-	return nil
-}
-
-func CopySymLink(source, dest string) error {
-	link, err := os.Readlink(source)
-	if err != nil {
-		return err
-	}
-	return os.Symlink(link, dest)
 }
--- go.mod
+++ go.mod
@@ -23,8 +23,10 @@ require (
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b // indirect
 	github.com/golang/groupcache v0.0.0-20190702054246-869f871628b6 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/otiai10/copy v1.14.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sync v0.3.0 // indirect
 	google.golang.org/genproto v0.0.0-20190425155659-357c62f0e4bb // indirect
 	google.golang.org/grpc v1.20.1 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
